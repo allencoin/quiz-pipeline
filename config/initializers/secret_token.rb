418c1c7713396448711283381c88e5f4afab7b81
Allen Coin <allencoin@gmail.com>
2015-04-09 04:15:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizPipeline::Application.config.secret_key_base = '3cbeb0244e0f32f35126aa23a38e2a49ac9788f00d45259ce14134ee339a5c9f2457591319aa727863cb6e4600d511110e0e7415f35969fbe1b2ef9bf17ac3f9'
