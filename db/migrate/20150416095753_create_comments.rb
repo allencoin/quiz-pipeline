295663ff8c675e32f5ecd6cbd2ae9ebd3d532da5
Allen Coin <allencoin@gmail.com>
2015-04-16 10:10:46 +0000

    fix database migration

--- db/migrate/20150416095753_create_comments.rb
+++ db/migrate/20150416095753_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
 
-      t.string: quote
-      t.string: author
+      t.string :quote
+      t.string :author
 
       t.timestamps
     end
